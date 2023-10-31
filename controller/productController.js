9b1fbbf5106bee0e1b0d49f0c357d24932021b9f
Serg-Ale <sergioalexandre0716@gmail.com>
2023-10-30 23:50:17 -0300

    implemented the mvc and errorMiddleware

--- controller/productController.js
+++ controller/productController.js
@@ -1,52 +1,54 @@
 const Product = require("../models/productModel");
+const asyncHandler = require('express-async-handler');
 
-const getProducts = async (req, res) => {
+const getProducts = asyncHandler(async (req, res) => {
     try {
         const products = await Product.find({});
         res.status(200).json(products);
     } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500);
+        throw new Error(error.message);
     }
-}
+});
 
-const getProduct = async (req, res) => {
+const getProduct = asyncHandler(async (req, res) => {
     try {
         const { id } = req.params;
         const product = await Product.findById(id);
         res.status(200).json(product);
     } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500);
+        throw new Error(error.message);
     }
-}
+});
 
-const createProduct = async (req, res) => {
+const createProduct = asyncHandler(async (req, res) => {
     try {
         const product = await Product.create(req.body);
         res.status(200).json(product);
     } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500);
+        throw new Error(error.message);
     }
-}
+});
 
-const updateProduct = async (req, res) => {
+const updateProduct = asyncHandler(async (req, res) => {
     try {
         const { id } = req.params;
         const product = await Product.findByIdAndUpdate(id, req.body);
         if (!product) {
-            res.status(404).json({ message: `cannot find any product with ID ${id}` });
+            res.status(404);
+            throw new Error(`cannot find any product with ID ${id}`);
         }
         const updatedProduct = await Product.findById(id);
         res.status(200).json(updatedProduct);
     } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500);
+        throw new Error(error.message);
     }
-}
+});
 
-const deleteProduct = async (req, res) => {
+const deleteProduct = asyncHandler(async (req, res) => {
     try {
         const { id } = req.params;
         const product = await Product.findByIdAndDelete(id);
@@ -55,10 +57,11 @@ const deleteProduct = async (req, res) => {
         }
         res.status(200).json(product);
     } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500);
+        throw new Error(error.message);
     }
-}
+});
+
 module.exports = {
     getProducts,
     getProduct,
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 require('dotenv').config();
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const productRoute = require('./routes/productRoute');
+const errorMiddleware = require("./middleware/errorMiddleware");
+
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL;
 
-
-const productRoute = require('./routes/productRoute');
-app.use("/api/products", productRoute);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    throw new Error('fake error');
+})
+
+// routes
+app.use("/api/products", productRoute);
+
+app.use(errorMiddleware);
 
 
 // Connect to MongoDB ant then start listen on port ${port}
